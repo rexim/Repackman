1d6ae8238569a62133c9ac11b268f2deaff7b0be
rexim <reximkut@gmail.com>
2012-09-18 12:22:51 +0700

    pom.xml: added lwjgl and junit to the dependencies; Main.java: added some test code.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,38 @@
+import org.lwjgl.LWJGLException;
+import org.lwjgl.input.Keyboard;
+import org.lwjgl.opengl.Display;
+import org.lwjgl.opengl.DisplayMode;
+import org.lwjgl.opengl.GL11;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Repackman");
+    public static void main(String[] args) throws LWJGLException{
+        // Initialization
+        Display.setDisplayMode(new DisplayMode(800, 600));
+        Display.setTitle("Repackman");
+        Display.create();
+
+        // Main loop
+        while(!Display.isCloseRequested()) {
+            Display.update();
+
+            // Draw test triangle
+            GL11.glBegin(GL11.GL_TRIANGLES);
+                GL11.glColor3d(1.0, 0.0, 0.0);
+                GL11.glVertex2d(-0.5, -0.5);
+
+                GL11.glColor3d(0.0, 1.0, 0.0);
+                GL11.glVertex2d(0.5, -0.5);
+
+                GL11.glColor3d(0.0, 0.0, 1.0);
+                GL11.glVertex2d(0.0, 0.5);
+            GL11.glEnd();
+
+            // Does user want to escape?
+            if(Keyboard.isKeyDown(Keyboard.KEY_ESCAPE))
+                break;
+        }
+
+        // Finalization
+        Display.destroy();
     }
 }
