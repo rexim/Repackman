15a901a066edf57520b70d81b4b3c057f9c786b7
Kolipass <kolipass@gmail.com>
2012-10-08 21:54:07 +0700

    meat of all Settings

--- src/main/java/SettingsManager.java
+++ src/main/java/SettingsManager.java
@@ -3,17 +3,19 @@
  * User: Kolipass
  * Date: 07.10.12
  * Time: 21:06
- * About information of this garbage comming soon
+ * Information about this garbage is coming soon?
  */
 public class SettingsManager {
     private static SettingsManager instance;
     private VideoSettings videoSettings;
     private SoundSettings soundSettings;
-    private Skin skinSettings;
+    private SkinSetting skinSettings;
     private ControlsSettings controlsSettings;
 
-    private SettingsManager() {
+    private String settingFileName;
 
+    private SettingsManager() {
+        settingFileName = getClass().getProtectionDomain().getCodeSource().getLocation().getPath() + "/settings.ini";
     }
 
     public static SettingsManager getInstance() {
@@ -28,42 +30,73 @@ public class SettingsManager {
     }
 
     public VideoSettings getVideoSettings() {
+        if (videoSettings == null) {
+            videoSettings = new VideoSettings(settingFileName);
+        }
         return videoSettings;
     }
 
     public void setVideoSettings(VideoSettings videoSettings) {
-        this.videoSettings = videoSettings;
+        if (!videoSettings.equals(this.videoSettings)) {
+            this.videoSettings = videoSettings;
+            videoSettings.writeSettingsToFile(settingFileName);
+        }
     }
 
     public SoundSettings getSoundSettings() {
+        if (soundSettings == null) {
+            soundSettings = new SoundSettings(settingFileName);
+        }
         return soundSettings;
     }
-         // especially for Rexim
+
+    // especially for Rexim
     public int getSoundVolume() {
+        if (soundSettings == null) {
+            getSoundSettings();
+        }
         return soundSettings.getSound_volume();
     }
     public int getMusic_volume() {
+        if (soundSettings == null) {
+            getSoundSettings();
+        }
         return soundSettings.getMusic_volume();
     }
 
     public void setSoundSettings(SoundSettings soundSettings) {
+        if (!soundSettings.equals(this.soundSettings)){
         this.soundSettings = soundSettings;
+            soundSettings.writeSettingsToFile(settingFileName);
+        }
     }
 
-    public Skin getSkinSettings() {
+    public SkinSetting getSkinSettings() {
+        if (skinSettings == null) {
+            skinSettings = new SkinSetting(settingFileName);
+        }
         return skinSettings;
     }
 
-    public void setSkinSettings(Skin skinSettings) {
-        this.skinSettings = skinSettings;
+    public void setSkinSettings(SkinSetting skinSettings) {
+        if (!skinSettings.equals(this.skinSettings)) {
+            this.skinSettings = skinSettings;
+            skinSettings.writeSettingsToFile(settingFileName);
+        this.skinSettings = skinSettings;                }
     }
 
     public ControlsSettings getControlsSettings() {
+        if (controlsSettings == null) {
+            controlsSettings = new ControlsSettings(settingFileName);
+        }
         return controlsSettings;
     }
 
     public void setControlsSettings(ControlsSettings controlsSettings) {
-        this.controlsSettings = controlsSettings;
+        if (!controlsSettings.equals(this.controlsSettings)) {
+            this.controlsSettings = controlsSettings;
+            controlsSettings.writeSettingsToFile(settingFileName);
+        this.controlsSettings = controlsSettings;   }
     }
 }
 
