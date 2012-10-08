42891d56a81910becbbc41f0e941088f26ad209a
rexim <reximkut@gmail.com>
2012-10-09 01:36:40 +0700

    Main: removed test code for settings classes

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,33 +1,30 @@
 import org.lwjgl.LWJGLException;
-import settings.SettingsManager;
+import org.lwjgl.input.Keyboard;
+import org.lwjgl.opengl.Display;
+import org.lwjgl.opengl.DisplayMode;
 
 public class Main {
     public static void main(String[] args) throws LWJGLException {
-        SettingsManager settingsManager = SettingsManager.getInstance();
-        System.out.println("Screen width = " + settingsManager.getVideoSettings().getScreenWidth());
-        System.out.println("Screen height = " + settingsManager.getVideoSettings().getScreenHeight());
-        System.out.println("Screen bpp = " + settingsManager.getVideoSettings().getBpp());
+        // Initialization
+        Display.setDisplayMode(new DisplayMode(800, 600));
+        Display.setTitle("Repackman");
+        Display.create();
 
-//        // Initialization
-//        Display.setDisplayMode(new DisplayMode(800, 600));
-//        Display.setTitle("Repackman");
-//        Display.create();
-//
-//        State currentState = new Splash();
-//
-//        // Main loop
-//        while(currentState != null && !Display.isCloseRequested()) {
-//            Display.update();
-//
-//            // Get next state
-//            currentState = currentState.next();
-//
-//            // Does user want to escape?
-//            if(Keyboard.isKeyDown(Keyboard.KEY_ESCAPE))
-//                break;
-//        }
-//
-//        // Finalization
-//        Display.destroy();
+        State currentState = new Splash();
+
+        // Main loop
+        while(currentState != null && !Display.isCloseRequested()) {
+            Display.update();
+
+            // Get next state
+            currentState = currentState.next();
+
+            // Does user want to escape?
+            if(Keyboard.isKeyDown(Keyboard.KEY_ESCAPE))
+                break;
+        }
+
+        // Finalization
+        Display.destroy();
     }
 }
